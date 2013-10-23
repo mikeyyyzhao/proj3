8595e5f80580ac4c420a921a2fbd7c643ac7a086
Michael Zhao <mchlzhao@gmail.com>
2013-10-22 20:19:20 -0700

    flipped kernel

--- part1.c
+++ part1.c
@@ -9,11 +9,30 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
     
+    //Flip the kernal here//
+
+    int xsize;
+    int ysize;
+    int temp;
+    for (ysize = 0; ysize<=KERNY; ysize++){
+    	for (xsize = 0; xsize<kern_cent_X; xsize++){
+    		temp = kernel[xsize][ysize];
+    		kernel[xsize][ysize] = kernel[KERNX-1-xsize][ysize];
+    		kernel[KERNY-1-xsize][ysize] = temp;
+    	}
+    }
+    for (xsize = 0; xsize<=KERNX; xsize++){
+    	for (ysize = 0; ysize<kern_cent_Y; ysize++){
+    		temp = kernel[xsize][ysize];
+    		kernel[xsize][ysize] = kernel[xsize][KERNY-1-ysize];
+    		kernel[xsize][KERNY-1-ysize] = temp;
+    	}
+    }    
     // main convolution loop
-	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
-			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
-				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
+	for(int x = 0; x < data_size_X; x+=KERNX){ // the x coordinate of the output location we're focusing on
+		for(int y = 0; y < data_size_Y; y+=KERNY){ // the y coordinate of theoutput location we're focusing on
+			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel flipped
+				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel is now flipped
 					// only do the operation if not out of bounds
 					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
 						//Note that the kernel is flipped
